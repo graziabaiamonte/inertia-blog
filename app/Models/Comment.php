26230025a94f16ad76b3a24a4c537971222f0484
Grazia <graziabaiamonte19@gmail.com>
2026-06-24 14:34:43 +0200

    plan eseguito fino al 5

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,7 +12,28 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 #[Fillable(['post_id', 'author_name', 'author_email', 'body', 'approved'])]
 /**
  * @method static \Illuminate\Database\Eloquent\Builder<static> approved()
- *
+ * @property int $id
+ * @property int $post_id
+ * @property string $author_name
+ * @property string $author_email
+ * @property string $body
+ * @property bool $approved
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Post $post
+ * @method static \Database\Factories\CommentFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Comment newModelQuery()
+ * @method static Builder<static>|Comment newQuery()
+ * @method static Builder<static>|Comment query()
+ * @method static Builder<static>|Comment whereApproved($value)
+ * @method static Builder<static>|Comment whereAuthorEmail($value)
+ * @method static Builder<static>|Comment whereAuthorName($value)
+ * @method static Builder<static>|Comment whereBody($value)
+ * @method static Builder<static>|Comment whereCreatedAt($value)
+ * @method static Builder<static>|Comment whereId($value)
+ * @method static Builder<static>|Comment wherePostId($value)
+ * @method static Builder<static>|Comment whereUpdatedAt($value)
+ * @mixin \Eloquent
  * @mixin IdeHelperComment
  */
 class Comment extends Model
